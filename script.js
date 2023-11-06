1679d3b273859e8cbd5191dbe8dfa63d84bf4dd6
viniciuss <146454578+niermidnight@users.noreply.github.com>
2023-11-06 09:40:37 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,71 @@
 const chatInput = document.querySelector(".chat-input textarea");
 const sendChatBtn = document.querySelector(".chat-input span");
 const chatbox = document.querySelector(".chatbox");
+const chatbotToggler = document.querySelector(".chatbot-toggler")
 
 let userMessage;
 
 const createChatLi = (message, className) => {
-    const chatLi = document.createElement("li");
-    chatLi.classList.add("chat", className);
-    let chatContent = className === "outgoing" ? `<p>${message}</p>` : `<span class="material-symbols-outlined">smart_toy</span><p>${message}</p>`;
-    chatLi.innerHTML = chatContent;
-    return chatLi;
+  const chatLi = document.createElement("li");
+  chatLi.classList.add("chat", className);
+  if (className === "incoming") {
+    chatLi.innerHTML = `<span class="material-symbols-outlined">smart_toy</span><p>${message}</p>`;
+  } else {
+    chatLi.innerHTML = `<p>${message}</p>`;
+  }
+  return chatLi;
+}
+
+const generateThinkingMessage = () => {
+  const thinkingLi = createChatLi("Pensando...", "incoming");
+  chatbox.appendChild(thinkingLi);
+  chatbox.scrollTo(0, chatbox.scrollHeight);
+  return thinkingLi;
 }
 
 const handleChat = () => {
-userMessage = chatInput.value.trim();
-if(!userMessage) return;
+  userMessage = chatInput.value.trim();
+  if (!userMessage) return;
 
-chatbox.appendChild(createChatLi(userMessage,"outgoing"));
+  chatbox.appendChild(createChatLi(userMessage, "outgoing"));
+  chatbox.scrollTo(0, chatbox.scrollHeight);
+  
+
+  const thinkingLi = generateThinkingMessage();
+
+  setTimeout(() => {
+    const botResponse = getBotResponse(userMessage);
+    thinkingLi.innerHTML = `<span class="material-symbols-outlined">smart_toy</span><p>${botResponse}</p>`;
+  }, 1200);
+
+  chatInput.value = "";
 }
 
-sendChatBtn.addEventListener("click",handleChat);
+const getBotResponse = (userMessage) => {
+  switch (userMessage) {
+    case "0":
+      return " Escolha uma das opções: <br><br>1 - Catalogar Sistemas "
+    case "1":
+      return "Você escolheu Catalogar Sistemas.<br><br>5 - Como Catalogar um novo Sistema.<br><br>";
+    case "2":
+      return "Você escolheu Consultar Sistemas.<br><br><br><br>";
+    case "3":
+      return "Você escolheu Exportar para o Excel.<br><br><br>";
+    default:
+      return "Desculpe, não entendi. Por favor, escolha uma das opções citadas." ;
+      chatbox.scrollTo(0, chatbox.scrollHeight);
+    
+  }
+
+}
+
+
+
+chatbotToggler.addEventListener("click", () =>document.body.classList.toggle("show-chatbot"));
+sendChatBtn.addEventListener("click", handleChat);
+chatInput.addEventListener("keydown", (event) => {
+  if (event.key === "Enter" && !event.shiftKey) {
+    event.preventDefault();
+    handleChat();
+  }
+});
